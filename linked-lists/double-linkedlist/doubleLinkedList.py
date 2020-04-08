ff410de218d002a955a1887122666b0542fa388c
ipeksargin <ipek.srgn@gmail.com>
2020-04-08 23:30:35 +0300

    double linked list

---
+++ linked-lists/double-linkedlist/doubleLinkedList.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class doubleLinkedList:
+    def __init__(self):
+        self.head == None
+
+    def append(self, new_data):
+        pass
+
+    def prepend(self,new_data): #add to front
+        pass
+
+    def print_list(self):
+        pass
